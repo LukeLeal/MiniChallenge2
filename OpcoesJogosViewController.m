3858ec5bf4d97adb921b21f08b0fdc5af474596c
Amanda <amanda96campos@gmail.com>
2015-03-23 16:25:08 -0300

    pag. de opções de jogos

---
+++ OpcoesJogosViewController.m
@@ -0,0 +1,45 @@
+//
+//  OpcoesJogosViewController.m
+//  Challenge 2
+//
+//  Created by Amanda Guimaraes Campos on 23/03/15.
+//  Copyright (c) 2015 Bepid. All rights reserved.
+//
+
+#import "OpcoesJogosViewController.h"
+
+@interface OpcoesJogosViewController ()
+
+@end
+
+@implementation OpcoesJogosViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)memoriaBotao:(id)sender {
+}
+
+- (IBAction)quizBotao:(id)sender {
+}
+
+- (IBAction)rouletteBotao:(id)sender {
+}
+@end
