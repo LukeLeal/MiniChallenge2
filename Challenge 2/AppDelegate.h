1a1ac105201a6c1a610d0cbf44def52e8a8b6987
LukeLeal <lukelealm@gmail.com>
2015-03-23 15:47:39 -0300

    Commit inicial

---
+++ Challenge 2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Challenge 2
+//
+//  Created by Lucas Leal Mendonça on 23/03/15.
+//  Copyright (c) 2015 Bepid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Challenge 2/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  Challenge 2
+//
+//  Created by Lucas Leal Mendonça on 23/03/15.
+//  Copyright (c) 2015 Bepid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
