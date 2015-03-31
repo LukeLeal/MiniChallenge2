82bc18183373bc8fe95c1b06c10360614bc287a1
André Lucas <AndreLucas@MackBooooook.local>
2015-03-31 16:09:41 -0300

    Lógica da memória

--- Challenge 2/MemoriaViewController.m
+++ Challenge 2/MemoriaViewController.m
@@ -10,6 +10,11 @@
 
 @interface MemoriaViewController (){
     NSArray *arrayBotoes;
+    int count;
+    NSMutableArray *selecionados;
+    //int hours, minutes, seconds;
+    double secondsLeft;
+    NSTimer *timer;
 }
 
 @end
@@ -22,9 +27,15 @@
     [super viewDidLoad];
     [self setTitle:@"Memória"];
     
+    secondsLeft = 60;
+    [self countdownTimer];
     arrayBotoes = [NSArray arrayWithObjects: carta1,carta2,carta3,carta4,carta5,carta6,carta7,carta8,carta9,carta10,carta11,carta12,nil];
-    
+}
+
+-(void)viewWillAppear:(BOOL)animated{
     [self preparaCartas];
+    count=0;
+    selecionados = [[NSMutableArray alloc]init];
 }
 
 - (void)viewWillDisappear:(BOOL)animated{
@@ -52,6 +63,8 @@
         [botao setTag:carta.tag];
         
         [botao.titleLabel setAlpha:0.0f];
+        botao.layer.borderWidth = 5.0;
+        botao.layer.borderColor = [UIColor grayColor].CGColor;
     }
 }
 
@@ -82,7 +95,91 @@
     [UIView transitionWithView:sender duration:0.5
                        options:UIViewAnimationOptionTransitionFlipFromRight animations:^{
               
-                           [sender setTitleColor:[UIColor whiteColor] forState:normal];
+//                           [sender setTitleColor:[UIColor whiteColor] forState:normal];
+                           [sender.titleLabel setAlpha:1.0f];
+                       } completion:^(BOOL finished){
+                           [sender setUserInteractionEnabled:NO];
+                           [self jogo:sender];
+                       }];
+}
+
+- (void)jogo: (UIButton *)sender{
+    if([selecionados count] == 0){
+        [selecionados addObject:sender];
+    }
+    
+    else{
+        if([self verificaTag:sender]){
+            [selecionados addObject:sender];
+            if ([selecionados count] == 3){
+                [self acerto];
+            }
+        }
+        else{
+            [selecionados addObject:sender];
+            [self retornaCartas];
+        }
+    }
+}
+
+//- (BOOL)continua{
+//    count++;
+//    if(count == 3){
+//        return false;
+//    }
+//    else{
+//        return true;
+//    }
+//}
+
+- (BOOL)verificaTag: (UIButton *)botao{
+    UIButton *last = [selecionados lastObject];
+    if(botao.tag == last.tag){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+- (void)retornaCartas{
+    
+    for (UIButton *b in selecionados){
+        [self animacaoErro:b];
+        [b setUserInteractionEnabled:YES];
+    }
+    
+    [selecionados removeAllObjects];
+}
+
+- (void)animacaoErro: (UIButton *)botao{
+    [botao setBackgroundColor:[UIColor blackColor]];
+    [botao.titleLabel setAlpha:0.0f];
+    [UIView transitionWithView:botao duration:0.5
+                       options:UIViewAnimationOptionTransitionFlipFromLeft animations:^{
                        } completion:nil];
+}
+
+- (void)acerto{
+    for (UIButton *b in selecionados){
+        [self animacaoAcerto:b];
+    }
+    [selecionados removeAllObjects];
+}
+
+- (void)animacaoAcerto: (UIButton *)botao{
+    [UIView beginAnimations:@"fadeInNewView" context:NULL];
+    [UIView setAnimationDuration:1.0];
+    botao.transform = CGAffineTransformMakeScale(1.8, 1.8);
+    botao.alpha = 1.0f;
+    [UIView commitAnimations];
+    
+    [UIView beginAnimations:@"fadeInNewView" context:NULL];
+    [UIView setAnimationDuration:1.0];
+    botao.transform = CGAffineTransformMakeScale(1,1);
+    botao.alpha = 1.0f;
+    botao.layer.borderWidth = 5.0;
+    botao.layer.borderColor = [UIColor purpleColor].CGColor;
+    [UIView commitAnimations];
 }
 @end
